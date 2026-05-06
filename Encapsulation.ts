d25306cb035963dbb1a2f5730c8034eed7d133a8
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-05-06 16:53:24 +0600

    Encapsulation

---
+++ Encapsulation.ts
@@ -0,0 +1,29 @@
+// access >>modify
+class BankAccount{
+     readonly userId:number;
+    public userName:string;
+   protected userBalance:number;
+    constructor(userId:number,userName:string,userBalance:number){
+        this.userBalance=userBalance;
+        this.userId=userId;
+         this.userName=userName;
+    }
+
+   private addBalance(balance:number){
+        this.userBalance=this.userBalance+balance;
+    }
+    callHiddenMethod(balance:number){
+        this.addBalance(balance);
+
+    }
+
+}
+
+class StudentBankAccount extends BankAccount{
+
+}
+
+const NapaAccount=new BankAccount(143,'Napa',100000);
+
+NapaAccount.addBalance(100000);
+console.log(NapaAccount);
